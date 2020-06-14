9ebbdfed4fdcd494f5e3fe6bf327ab3cac54f1c6
Hugo Ferreira <hugoferreira@outlook.com>
2020-06-15 00:03:07 +0100

    Server info, share url and alerts

--- speedtest-exporter.sh
+++ speedtest-exporter.sh
@@ -3,14 +3,17 @@
 function printMetric {
     echo "# HELP $1 $2"
     echo "# TYPE $1 $3"
-    echo "$1 $4"
+    echo "$1$5 $4"
 }
 
-while IFS=$'\t' read -r servername serverid latency jitter packetloss download upload downloadedbytes uploadedbytes share_url; do
+while IFS=$'\t' read -r servername serverid latency jitter packetloss download upload downloadedbytes uploadedbytes shareurl; do
+    printMetric "speedtest_servername_info" "Server Name" "gauge" "1" "{servername=\"$servername\"}"
+    printMetric "speedtest_serverid_info" "Server ID" "gauge" "1" "{serverid=\"$serverid\"}"
     printMetric "speedtest_latency_seconds" "Latency" "gauge" "$latency"
     printMetric "speedtest_jittter_seconds" "Jitter" "gauge" "$jitter"
     printMetric "speedtest_download_bytes" "Download Speed" "gauge" "$download"
     printMetric "speedtest_upload_bytes" "Upload Speed" "gauge" "$upload"
     printMetric "speedtest_downloadedbytes_bytes" "Downloaded Bytes" "gauge" "$downloadedbytes"
     printMetric "speedtest_uploadedbytes_bytes" "Uploaded Bytes" "gauge" "$uploadedbytes"
+    printMetric "speedtest_shareurl_info" "Share URL" "gauge" "1" "{shareurl=\"$shareurl\"}"
 done < <(/usr/bin/speedtest -f tsv)
